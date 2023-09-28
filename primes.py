8bb2bef74bb656f802e259e9750afc43e02024eb
Nithursan <nithursan.muraleetharan@kcl.ac.uk>
2023-09-28 14:20:31 +0100

    Add solution

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    number = 2
+    count = 0
+    while(len(list) < number_of_primes):
+        isPrime = True
+        i = 1
+        while(isPrime and i < number):
+            if(number % i == 0 and i != 1):
+                isPrime = False
+            i += 1
+        if(isPrime):
+            list.append(number)
+        number += 1
     return list
